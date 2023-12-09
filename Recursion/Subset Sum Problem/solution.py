860789dd6c4d8a85a58b0c9b5ee245be7cea1dca
Akshay Parihar <129431619+Akshayparihar07@users.noreply.github.com>
2023-12-10 03:06:51 +0530

    Update solution.py

--- Recursion/Subset Sum Problem/solution.py
+++ Recursion/Subset Sum Problem/solution.py
@@ -1,8 +1,8 @@
 class Solution:
-    def isSubsetSum(self, N, arr, sum):
-        def solve(i, sumi):
+    def isSubsetSum(self, N, arr, Sum):
+        def solve(i, current_sum):
             # If the current subset sum equals the target sum
-            if sumi == sum:
+            if current_sum == Sum:
                 return True
                 
             # If we have processed all elements in the array
@@ -11,7 +11,7 @@ class Solution:
 
             # Try including the current element in the subset
             for j in range(i, N):
-                if solve(j + 1, sumi + arr[j]):
+                if solve(j + 1, current_sum + arr[j]):
                     return True
 
         # Start the recursive function with initial values

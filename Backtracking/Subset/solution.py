5f4a0735fc53d274bb1a81411a3e39681b75a419
Akshay Parihar <129431619+Akshayparihar07@users.noreply.github.com>
2023-12-10 01:37:52 +0530

    Update solution.py

--- Backtracking/Subset/solution.py
+++ Backtracking/Subset/solution.py
@@ -1,35 +1,20 @@
-from typing import List
+def solve(nums, i, subset, ans):
+    # Append the current subset to the result
+    ans.append(subset[:])  
 
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        # Initialize an empty list to store the final subsets
-        result = []
-        
-        # Call the recursive helper function to generate subsets
-        self._generate_subsets(nums, 0, [], result)
-        
-        # Return the final list of subsets
-        return result
+    # Explore all possible subsets by recursively adding elements
+    for j in range(i, len(nums)):
+        subset.append(nums[j])  
+        solve(nums, j + 1, subset, ans)
+        subset.pop()
 
-    def _generate_subsets(self, nums, index, current_subset, result):
-        """
-        Recursive helper function to generate subsets of the given list of numbers.
-
-        Parameters:
-        - nums: The input list of numbers
-        - index: The current index in the iteration
-        - current_subset: The current subset being generated
-        - result: The list to store the final subsets
-        """
-
-        # Base case: if the current index is equal to the length of the input list,
-        # then we have reached the end of the list, and the current subset is a valid subset.
-        if index == len(nums):
-            result.append(current_subset[:])  # Append a copy of the current subset to the result
-            return
-
-        # Include the current element at the index in the subset and recurse
-        self._generate_subsets(nums, index + 1, current_subset + [nums[index]], result)
-
-        # Exclude the current element at the index and recurse
-        self._generate_subsets(nums, index + 1, current_subset, result)
+# Main function to generate all subsets of a given list of numbers
+def generateSubsets(nums):
+    ans = []        # Resultant list to store all subsets
+    subset = []     # Current subset being constructed
+    i = 0           # Starting index for subset generation
+    
+    # Start the recursive subset generation process
+    solve(nums, i, subset, ans)
+    
+    return ans      # Return the final list of subsets

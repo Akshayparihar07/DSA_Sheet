ad4948778ba74fec25b541bcd484b457241cbfe5
Akshay Parihar <129431619+Akshayparihar07@users.noreply.github.com>
2023-12-26 01:42:17 +0530

    Update solution_TD.py

--- Dynamic Programming/Longest Common Subsequence/solution_TD.py
+++ Dynamic Programming/Longest Common Subsequence/solution_TD.py
@@ -1,34 +1,33 @@
 def solve(text1, text2, i, j, memo):
-    # Base case: If either of the indices is less than 0, no more characters to compare.
+    # Base case: If either string has reached the beginning, return 0
     if i < 0 or j < 0:
         return 0
 
-    # Check if the result for the current indices is already computed and stored in memo.
+    # Check if the result for the current state is already memoized
     if memo[i][j] != -1:
         return memo[i][j]
 
     match = 0
-    # Check if the current characters in both texts match.
+    # Check if the characters at the current positions match
     if text1[i] == text2[j]:
-        # If there's a match, recursively call solve for the previous characters in both texts.
+        # If there is a match, add 1 to the result and move to the previous characters
         match = solve(text1, text2, i-1, j-1, memo) + 1
 
-    # Recursively call solve for the case where the current character in text2 is skipped.
-    match1 = solve(text1, text2, i, j-1, memo)
-    # Recursively call solve for the case where the current character in text1 is skipped.
-    match2 = solve(text1, text2, i-1, j, memo)
+    # If there is no match, explore two possibilities: moving one character back in either text
+    not_match1 = solve(text1, text2, i, j-1, memo)  # Move back in text2
+    not_match2 = solve(text1, text2, i-1, j, memo)  # Move back in text1
 
-    # Store the maximum of the three cases in memo for future reference.
-    memo[i][j] = max(match, match1, match2)
+    # Update memo table with the maximum result among the three possibilities
+    memo[i][j] = max(match, not_match1, not_match2)
 
-    # Return the maximum match count for the current indices.
+    # Return the result for the current state
     return memo[i][j]
 
-# Initialize memoization table with -1 for each combination of indices.
-memo = [[-1] * len(text2) for _ in range(len(text1))]
+# Initialize memoization table with -1 values
+memo = [[-1]*len(text2) for _ in range(len(text1))]
 
-# Start solving the problem from the last characters of both texts.
+# Start the recursive function with the last indices of both texts
 result = solve(text1, text2, len(text1)-1, len(text2)-1, memo)
 
-# Return the final result.
+# Return the final result
 return result
